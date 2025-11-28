e19b71594b45d537165f6e03c761ffd5d85dc09d
Merijn <1096446@hr.nl>
2025-11-28 10:39:13 +0100

    Add managers for tracks, data, races, and leaderboards in Main class

--- src/main/java/me/Codex/vvKart/Main.java
+++ src/main/java/me/Codex/vvKart/Main.java
@@ -1,17 +1,57 @@
 package me.Codex.vvKart;
 
+import me.Codex.vvKart.Manages.DataManager;
+import me.Codex.vvKart.Manages.LeaderBoardManager;
+import me.Codex.vvKart.Manages.RaceManager;
+import me.Codex.vvKart.Manages.TrackManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    private TrackManager trackManager;
+    private DataManager dataManager;
+    private RaceManager raceManager;
+    private LeaderBoardManager leaderBoardManager;
+
+    @Override
+    public void onLoad() {
+
+        saveDefaultConfig();
+
+        trackManager = new TrackManager(this);
+        dataManager = new DataManager(this);
+        dataManager.loadAll();
+    }
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        raceManager = new RaceManager(this);
+        leaderBoardManager = new LeaderBoardManager(this);
+        getLogger().info("Plugin enabled!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public TrackManager getTrackManager() {
+        return trackManager;
+    }
+
+    public DataManager getDataManager() {
+        return dataManager;
+    }
+
+    public RaceManager getRaceManager() {
+        return raceManager;
+    }
+
+    public LeaderBoardManager getLeaderBoardManager() {
+        return leaderBoardManager;
+    }
+
+    public static Main getInstance() {;
+        return JavaPlugin.getPlugin(Main.class);
     }
 }
