57eafc53c6e660d154c0dfbcffdefda907c61634
Merijn <1096446@hr.nl>
2025-11-28 10:39:54 +0100

    Add PlayerLeaveEvent to handle player removal from races on quit

---
+++ src/main/java/me/Codex/vvKart/Events/PlayerLeaveEvent.java
@@ -0,0 +1,20 @@
+package me.Codex.vvKart.Events;
+
+import me.Codex.vvKart.Main;
+import me.Codex.vvKart.Manages.RaceManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerLeaveEvent implements Listener {
+
+    @EventHandler
+    public void onPlayerLeave(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+        RaceManager raceManager = Main.getInstance().getRaceManager();
+        if (raceManager.isInRace(player)) {
+            raceManager.removeFromRace(player);
+        }
+    }
+}
